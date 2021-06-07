9d02ed859c09cf743edf48bb48c66b687b4aa092
Pedro Ricardo <pedro.ricardo123go@gmail.com>
2021-06-06 23:35:25 -0300

    v2]

--- src/mock/data.js
+++ src/mock/data.js
@@ -62,15 +62,7 @@ export const projectsData = [
     url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: 'https://snack.expo.io/@trickstahs/pedra-papel-tesoura-lagarto-e-spock',
-    repo: 'https://snack.expo.io/@trickstahs/pedra-papel-tesoura-lagarto-e-spock', // if no repo, the button will not show up
-  },
+  
 ];
 
 // CONTACT DATA
